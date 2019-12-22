6392161e0d8a0bb525e723c7c8d962551abb776b
Emilio Garcia <emilio.garcia@ieec.cat>
2019-12-22 20:56:14 +0100

    Initial header file.

---
+++ emiliogq-raffle-wp-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Emiliogq Raffle Wordpress Plugin
+ * Plugin URI:        http://web.emiliogq.com/projects/raffle-wp-plugin/
+ * Description:       Wordpress plugin used for making raffles among the wordpress users.
+ * Version:           0.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Emilio Garcia
+ * Author URI:        http://web.emiliogq.com/
+ * Text Domain:       emiliogq-raffle-wp-plugin
+ * Domain Path:       /languages
+ */
+?>
